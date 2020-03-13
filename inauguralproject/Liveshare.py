5c00631ce562d1a00d96e57974571463b5f783d9
Profz <jonathan.thor.isaksen@gmail.com>
2020-03-13 15:11:46 +0100

    Small changes to question 1, still not complete

--- inauguralproject/Liveshare.py
+++ inauguralproject/Liveshare.py
@@ -1,4 +1,12 @@
+# importint packages
 import numpy as np
+import itertools as it
+from scipy import optimize
 
+# importing pyplot and chossing style.
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+
+# Question 1
 # Explain how to solve the model.
 
