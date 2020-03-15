f5f9cb5b347f97f77c9bff7a8ce8daa8055b7d7c
Profz <jonathan.thor.isaksen@gmail.com>
2020-03-15 14:25:58 +0100

    added solution to question 2 in LiveShare

--- inauguralproject/Liveshare.py
+++ inauguralproject/Liveshare.py
@@ -4,6 +4,11 @@ import numpy as np
 import itertools as it
 from scipy import optimize
 
+%matplotlib inline
+import matplotlib.pyplot as plt # baseline modul
+from mpl_toolkits.mplot3d import Axes3D # for 3d figures
+plt.style.use('seaborn-whitegrid') # whitegrid nice with 3d
+
 # Defining a function that will solve the model.
 def solver(m,v,epsilon,tao0,tao1,kappa,w,cl,f):
 
@@ -39,3 +44,41 @@ def print_solution(c,l,u):
     print(f'l = {l:.8f}')
     print(f'u = {u:.8f}')
 
+# Opgave 2
+
+m = 1
+v = 10
+epsilon = 0.3
+tao0 = 0.4
+tao1 = 0.1
+kappa = 0.4
+w = 1
+cl = (1,1)
+
+#Defining model
+def f(c = cl[0], l = cl[1], v = v, epsilon = epsilon):
+    return np.log(c) - v*(l**(1+1/epsilon))/(1+1/epsilon)
+
+
+
+
+
+
+
+
+
+# solution to question 2 :D
+
+wliste = np.arange(0.5, 1.5, 0.01).tolist()
+uliste = []
+for w in wliste:
+    sol = solver(m,v,epsilon,tao0,tao1,kappa,w,cl,f)
+    c = sol.x[0]
+    l = sol.x[1]
+    u = f(c,l)
+    uliste.append(u)
+print(uliste)
+print(wliste)
+fig = plt.figure(figsize=(10,4))# figsize is in inches...
+ax = fig.add_subplot(1,2,1)
+ax.plot(wliste,uliste) # create surface plot in the axis
